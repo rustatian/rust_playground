11dc23d8d64ee9c1529c8e16d5ae67d6bb745026
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-07-14 20:42:35 +0300

    $(init 0)

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,35 +1,45 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        let mut guess: String = String::new();
+        println!("Please input your guess.");
 
-    let mut guess:String = String::new();
-
-    match io::stdin().read_line(&mut guess) {
-        Ok(_n) => {
-            //println!("{} bytes read", n);
-            println!("{}", guess);
+        match io::stdin().read_line(&mut guess) {
+            Ok(_n) => {
+                //println!("{} bytes read", n);
+                println!("{}", guess);
+            }
+            Err(error) => {
+                println!("Failed to read line");
+                println!("error: {}", error)
+            }
         }
-        Err(error) => {
-            println!("Failed to read line");
-            println!("error: {}", error)
+        //.expect("Failed to read line");
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Too small!");
+            }
+            Ordering::Greater => {
+                println!("Too big!");
+            }
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
-    }
-    //.expect("Failed to read line");
-    println!("You guessed: {}", guess);
-
-    let guess :u32 = guess.trim().parse().expect("Please, type a number");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => {},
-        Ordering::Greater => {},
-        Ordering::Equal => {},
     }
 }
---
+++ variables/target/debug/deps/variables-1283cc645fd04dc4.d
@@ -0,0 +1,5 @@
+/Users/0xdev/Projects/repo/rust_playground/variables/target/debug/deps/variables-1283cc645fd04dc4: src/main.rs
+
+/Users/0xdev/Projects/repo/rust_playground/variables/target/debug/deps/variables-1283cc645fd04dc4.d: src/main.rs
+
+src/main.rs:
