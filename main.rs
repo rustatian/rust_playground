24ef8e60636373344819938dc50d9c6a99c5d0b8
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-08-06 20:02:39 +0300

    Now added delete for real

--- main.rs
+++ main.rs
@@ -1,3 +1,11 @@
 fn main() {
+    let mut x = Some(2);
 
+    match x.take() {
+        Some(2) => { println!("2") }
+        None => { println!("None") }
+        _ => { println!("error") }
+    };
+
+    assert_eq!(x, Some(2));
 }
