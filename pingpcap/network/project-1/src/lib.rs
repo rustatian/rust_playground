8c4661e7bea26e14cdd88dbe70ad488d3e683b99
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-09-28 16:58:51 +0300

    finished with p1

--- pingpcap/network/project-1/src/lib.rs
+++ pingpcap/network/project-1/src/lib.rs
@@ -1,23 +1,34 @@
-pub struct KvStore {
+use std::collections::HashMap;
+use std::ops::Deref;
 
+pub struct KvStore {
+    data: HashMap<String, String>,
 }
 
 impl KvStore {
     pub fn new() -> KvStore {
-        KvStore{
-
+        KvStore {
+            data: HashMap::new(),
         }
     }
 
     pub fn get(&mut self, key: String) -> Option<String> {
-        None
+        match self.data.get(&key) {
+            Some(val) => Some(val.parse::<String>().unwrap()),
+            _ => None,
+        }
     }
 
     pub fn set(&mut self, key: String, value: String) {
-
+        self.data.insert(key, value);
     }
 
     pub fn remove(&mut self, key: String) {
-
+        match self.data.get(&key) {
+            Some(val) => {
+                self.data.remove(&key);
+            }
+            _ => {}
+        }
     }
 }
