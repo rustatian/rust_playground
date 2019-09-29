37aa0c8f6efcc06d8b08019e87d4f54074e9d49b
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-09-29 18:46:17 +0300

    the magic is real

--- http_thread_pool/src/main.rs
+++ http_thread_pool/src/main.rs
@@ -1,31 +1,20 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
-use std::thread;
-
+use http_thread_pool::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    let pool = ThreadPool::new(100);
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(strm) => {
-                thread::spawn(|| {
-                    handle_connection(strm);
-                });
-            }
-            Err(e) => {
-                println!("couldn't get client: {:?}", e);
-            }
-        }
-//        let stream = stream.unwrap();
-//        thread::spawn(|| {
-//            handle_connection(stream);
-//        });
+        pool.execute(move || {
+            handle_connection(stream.unwrap());
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let response = b"HTTP/1.1 200 OK\r\n\r\n";
-    stream.write(response).unwrap();
+    let _num = stream.write(response).unwrap();
     stream.flush().unwrap();
 }
