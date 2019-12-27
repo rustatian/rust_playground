c75b9e321499ceb066abadb9c12403522630c52d
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-12-27 23:45:39 +0300

    And if you ask me how I'm feeling

--- rust_in_action/src/main.rs
+++ rust_in_action/src/main.rs
@@ -7,29 +7,22 @@ mod grep_lite_v1;
 mod lang_foundations;
 
 fn main() {
-    grep_lite_v1::using_regexp();
+    let n: f64 = 42.42;
+
+    let (signbit, exponent, fraction) = deconstruct_f32(n);
+
+    println!("{} -> [sign:{}, exponent:{}]", n, signbit, exponent);
 }
 
-fn types_declaring() {
-    let a = 10;
-    let b = Box::new(20);
-    let c = Rc::new(Box::new(30));
-    let d = Arc::new(Mutex::new(40));
-}
-
-pub fn add_with_lifetimes<'a, 'b>(i: &'a i32, j: &'b i32) -> i32 {
-    5
-}
-
-
-fn deconstruct_f32(n: f32) -> (u32, u32, u32) {
-    let n_:u32 = unsafe {
+fn deconstruct_f32(n: f64) -> (u64, u64, u64) {
+    let n_: u64 = unsafe {
         std::mem::transmute(n)
     };
 
-    let sign = (n_ >> 31) & 1;
-    let exponent = (n_ >> 32) & 0xff;
-    let fraction = 0b00000000_01111111_11111111_11111111 & n_;
+    let sign = (n_ >> 63) & 1;
+    let exponent = (n_ >> 52) & 0xff;
+    let fraction = 0b00000000_01111111_11111111_11111111_11111111_11111111_11111111_11111111 & n_;
 
     (sign, exponent, fraction)
 }
+
