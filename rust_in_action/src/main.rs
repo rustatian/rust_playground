c7c434a34a1e7da285f13277dc81d0e31fac4706
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-12-28 20:53:17 +0300

    move your body every every body

--- rust_in_action/src/main.rs
+++ rust_in_action/src/main.rs
@@ -18,6 +18,7 @@ fn deconstruct_f32(n: f64) -> (u64, u64, u64) {
     let n_: u64 = unsafe {
         std::mem::transmute(n)
     };
+    
 
     let sign = (n_ >> 63) & 1;
     let exponent = (n_ >> 52) & 0xff;
