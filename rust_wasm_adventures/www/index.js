29f1831820ab1d91fd95fe33558c5dbb89e5950f
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-01-07 18:43:59 +0300

    derpherp

---
+++ rust_wasm_adventures/www/index.js
@@ -0,0 +1,13 @@
+import { Universe } from "rust_wasm_adventures";
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
