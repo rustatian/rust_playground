184e5d644fa68b1f447873e3de2a0f25fb7aa87b
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-01-20 10:24:38 +0300

    We'll figure it out on Monday

--- mastering_rust/src/main.rs
+++ mastering_rust/src/main.rs
@@ -1,6 +1,3 @@
-use crate::threading::shared_state;
-use crate::tokio::tokio_start;
-
 mod mutex;
 mod chapter1;
 mod strings;
@@ -10,14 +7,7 @@ mod tokio;
 
 #[allow(dead_code)]
 fn main() {
-    let mut v = vec![1, 2, 3];
-    let u: Vec<_> = v.drain(..1).collect();
-    assert_eq!(v, &[1]);
-    assert_eq!(u, &[2, 3]);
 
-    // A full range clears the vector
-    v.drain(..);
-    assert_eq!(v, &[]);
 }
 
 
