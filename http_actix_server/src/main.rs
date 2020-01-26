da1bcca6bd27a8f87aae2c7ede63df703d5bfb2f
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-01-26 23:24:29 +0300

    commented out failing tests

--- http_actix_server/src/main.rs
+++ http_actix_server/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_rt::System;
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+
+fn main()  {
+    some_func();
+}
+
+#[inline(always)]
+fn some_func() {
+    let sys = System::new("http-server");
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get())
+            .route("/{name}", web::get().to(greet))
+    }).bind("127.0.0.1:8000").unwrap()
+        .workers(4).run();
+    sys.run();
 }
