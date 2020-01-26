de0448c87420e05851d8ad4b05d36f7edad143fc
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-01-26 17:40:24 +0300

    Misc. fixes

--- p_kernel/src/main.rs
+++ p_kernel/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(core_intrinsics)]
+#![no_std]
+#![no_main]
+
+use core::intrinsics;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[no_mangle]
+fn panic(_info: &PanicInfo) -> ! {
+    unsafe {
+        intrinsics::abort();
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let framebuffer = 0xb8000 as *mut u8;
+    unsafe {
+        framebuffer.offset(1).write_volatile(0x30);
+    }
+    loop {}
 }
