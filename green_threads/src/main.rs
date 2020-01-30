7c848ee8b7c3c8bb5706506da958216432fb9955
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-01-30 18:27:18 +0300

    this is how we generate our shit.

--- green_threads/src/main.rs
+++ green_threads/src/main.rs
@@ -1,11 +1,33 @@
 #![feature(asm)]
 
+use std::ops::BitAnd;
+
 const SSIZE: isize = 48;
 
+#[derive(Debug, Default)]
+#[repr(C)]
 struct ThreadContext {
     rsp: u64,
 }
 
+fn hello() -> ! {
+    println!("I LOVE WAKING UP ON A NEW STACK!");
+
+    loop {}
+}
+
 fn main() {
 
+}
+
+unsafe fn gt_switch(new: *const ThreadContext) {
+    asm!("
+        mov     0x00($0), %rsp
+        ret
+       "
+    :
+    : "r"(new)
+    :
+    : "alignstack" // it will work without this now, will need it later
+    );
 }
