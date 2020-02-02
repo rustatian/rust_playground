755452467a4ce2bd64fff06cdfe2934e6a002e1c
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-02-02 22:30:04 +0300

    :q!

--- custom_block_on/src/main.rs
+++ custom_block_on/src/main.rs
@@ -1,3 +1,21 @@
+use std::task::Context;
+use futures::Future;
+use futures::task::Poll;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+
+
+
+}
+
+fn block_on<F:Future>(future: F) -> F::Output {
+    let waker = todo!();
+    let cx = &mut Context::from_waker(&waker);
+    loop {
+        match future.as_mut().poll(cx) {
+           Poll::Ready(output) => return output,
+            Poll::Pending => thread::park(),
+        }
+    }
 }
