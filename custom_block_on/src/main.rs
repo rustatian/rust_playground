6a95c39dca5fd39100e117b555031e39309fdcbd
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-02-02 23:08:30 +0300

    stuff

--- custom_block_on/src/main.rs
+++ custom_block_on/src/main.rs
@@ -1,20 +1,18 @@
 use std::task::Context;
-use futures::Future;
-use futures::task::Poll;
+use std::task::Poll;
 use std::thread;
+use std::future::Future;
 
-fn main() {
+fn main() {}
 
-
-
-}
-
-fn block_on<F:Future>(future: F) -> F::Output {
-    let waker = todo!();
+fn block_on<F: Future>(future: F) -> F::Output {
+    pin_utils::pin_mut!(future);
+    let thread = thread::current();
+    let waker = async_task::waker_fn(move || thread.unpark());
     let cx = &mut Context::from_waker(&waker);
     loop {
         match future.as_mut().poll(cx) {
-           Poll::Ready(output) => return output,
+            Poll::Ready(output) => return output,
             Poll::Pending => thread::park(),
         }
     }
