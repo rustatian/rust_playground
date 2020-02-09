d1ac8221164a914e7ecce488a526ac290a8df064
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-02-09 22:52:45 +0300

    lol

--- custom_executor/src/main.rs
+++ custom_executor/src/main.rs
@@ -1,5 +1,8 @@
 use futures::Future;
-use async_task::JoinHandle;
+use std::pin::Pin;
+use futures::channel::oneshot;
+use std::sync::atomic::AtomicUsize;
+use std::sync::Mutex;
 
 fn main() {
     futures::executor::block_on(async {
@@ -8,9 +11,31 @@ fn main() {
     });
 }
 
+// task consist of future and it's state
+// we need to know whether the task is scheduled for running
+struct Task {
+    state: AtomicUsize,
+    // Output is () because the `spawn()` function has wrapped the original
+    // future into one that sends the output into the oneshot channel and then
+    // simply returns with `()`
+    future: Mutex<Pin<Box<dyn Future<Output=()> + Send>>>,
+}
+
+// temporary
+type JoinHandle<R> = Pin<Box<dyn Future<Output=R> + Send>>;
+
 fn spawn<F, R>(future: F) -> JoinHandle<R> where
     F: Future<Output=R> + Send + 'static,
     R: Send + 'static
 {
-    todo!()
+    let (s, r) = oneshot::channel();
+    let future = async move {
+        let _ = s.send(future.await);
+    };
+
+    todo!();
+
+    Box::pin(async {
+        r.await.unwrap()
+    })
 }
