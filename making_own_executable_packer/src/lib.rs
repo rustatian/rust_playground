ced3bf8b5e884d8d39af99b6f26dc33d4c90aee3
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-03-24 23:22:11 +0300

    Don't Ask Me, I Have No Idea Why This Works Either

---
+++ making_own_executable_packer/src/lib.rs
@@ -0,0 +1,27 @@
+mod parse;
+
+#[derive(Debug)]
+pub struct File {}
+
+impl File {
+    const ELF_MAGIC: &'static [u8] = &[0x7f, 0x45, 0x4c, 0x46];
+
+    pub fn parse(i: parse::Input) -> parse::Result<Self> {
+        use nom::{
+            bytes::complete::{tag, take},
+            error::context,
+            sequence::tuple,
+        };
+
+        let (i, _) = tuple((
+            context("Magic", tag(Self::ELF_MAGIC)),
+            context("Class", tag(&[0x2])),
+            context("Endianness", tag(&[0x1])),
+            context("Version", tag(&[0x1])),
+            context("OS ABI", tag(&[0x0])),
+            context("Padding", take(8_usize)),
+        ))(i)?;
+
+        Ok((i, Self {}))
+    }
+}
---
+++ making_own_executable_packer/src/parse.rs
@@ -0,0 +1,2 @@
+pub type Input<'a> = &'a [u8];
+pub type Result<'a, O> = nom::IResult<Input<'a>, O, nom::error::VerboseError<Input<'a>>>;
