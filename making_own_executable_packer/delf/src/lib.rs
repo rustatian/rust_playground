e599906636ad3ab3c1a00f5c7c387ea002c7feca
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-03-26 00:01:31 +0300

    bump to 0.0.3-dev:wq

--- making_own_executable_packer/delf/src/lib.rs
+++ making_own_executable_packer/delf/src/lib.rs
@@ -26,3 +26,35 @@ impl File {
         Ok((i, Self {}))
     }
 }
+
+// e-type, 16 byte
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u16)]
+pub enum Type {
+    None = 0x0,
+    Rel = 0x1,
+    Exec = 0x2,
+    Dyn = 0x3,
+    Core = 0x4,
+    Loos = 0xfe00,
+    Hios = 0xfeff,
+    Loproc = 0xff00,
+    Hiproc = 0xffff,
+}
+
+impl Type {
+    pub fn from_u16(x: u16) -> Option<Self> {
+        match x {
+            0 => Some(Self::None),
+        }
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn type_to_u16() {
+        assert_eq!(super::Type::Dyn as u16, 0x3)
+    }
+}
--- making_own_executable_packer/elk/src/main.rs
+++ making_own_executable_packer/elk/src/main.rs
@@ -2,7 +2,7 @@ use std::{env, error::Error, fs};
 fn main() -> Result<(), Box<dyn Error>> {
     let input_path = env::args().skip(1).next().expect("usage: elk FILE");
     let input = fs::read(&input_path)?;
-    making_own_executable_packer::File::parse(&input[..]).map_err(|e| format!("{:?}", e))?;
+    delf::File::parse(&input[..]).map_err(|e| format!("{:?}", e))?;
     println!("input is a supported ELF file");
     Ok(())
 }
