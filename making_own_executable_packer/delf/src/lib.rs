fa32d1fdc1be7a320490f9abbcba4b957fd3e8eb
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-03-26 15:55:32 +0300

    Replace all whitespaces with tabs.

--- making_own_executable_packer/delf/src/lib.rs
+++ making_own_executable_packer/delf/src/lib.rs
@@ -1,7 +1,12 @@
+use std::convert::TryFrom;
+
 mod parse;
 
 #[derive(Debug)]
-pub struct File {}
+pub struct File {
+    pub r#type: Type,
+    pub machine: Machine,
+}
 
 impl File {
     const ELF_MAGIC: &'static [u8] = &[0x7f, 0x45, 0x4c, 0x46];
@@ -11,8 +16,9 @@ impl File {
             bytes::complete::{tag, take},
             error::context,
             sequence::tuple,
+            combinator::map,
+            number::complete::le_u16,
         };
-
         // 7f45 4c46 0201 0100 0000 0000 0000 0000
         let (i, _) = tuple((
             context("Magic", tag(Self::ELF_MAGIC)),
@@ -23,7 +29,17 @@ impl File {
             context("Padding", take(8_usize)),
         ))(i)?;
 
-        Ok((i, Self {}))
+        let (i, (r#type, machine)) = tuple((
+            context("Type", map(le_u16, |x| Type::from_u16(x))),
+            context("Machine", map(le_u16, |x| Machine::from_u16(x),
+            )),
+        ))(i)?;
+
+        let t = r#type;
+        let m = machine;
+
+        let res = Self { machine: m.unwrap(), r#type: t.unwrap() };
+        Ok((i, res))
     }
 }
 
@@ -36,20 +52,39 @@ pub enum Type {
     Exec = 0x2,
     Dyn = 0x3,
     Core = 0x4,
-    Loos = 0xfe00,
-    Hios = 0xfeff,
-    Loproc = 0xff00,
-    Hiproc = 0xffff,
 }
 
 impl Type {
     pub fn from_u16(x: u16) -> Option<Self> {
         match x {
             0 => Some(Self::None),
+            1 => Some(Self::Rel),
+            2 => Some(Self::Exec),
+            3 => Some(Self::Dyn),
+            4 => Some(Self::Core),
+            _ => None,
         }
-
     }
 }
+
+// e-machine
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u16)]
+pub enum Machine {
+    X86 = 0x03,
+    X86_64 = 0x3e,
+}
+
+impl Machine {
+    pub fn from_u16(m: u16) -> Option<Self> {
+        match m {
+            0x03 => Some(Self::X86),
+            0x3e => Some(Self::X86_64),
+            _ => None,
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -57,4 +92,50 @@ mod tests {
     fn type_to_u16() {
         assert_eq!(super::Type::Dyn as u16, 0x3)
     }
+
+    #[test]
+    fn type_from_u16() {
+        assert_eq!(super::Type::from_u16(0x3), Some(super::Type::Dyn));
+        assert_eq!(super::Type::from_u16(0xf00d), None)
+    }
+
+    fn machine_from_u16() {
+        assert_eq!(super::Machine::X86_64 as u16, 0x3e);
+        assert_eq!(super::Machine::from_u16(0x3e), Some(super::Machine::X86_64));
+        assert_eq!(super::Machine::from_u16(0xfa), None)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- making_own_executable_packer/elk/src/main.rs
+++ making_own_executable_packer/elk/src/main.rs
@@ -2,7 +2,7 @@ use std::{env, error::Error, fs};
 fn main() -> Result<(), Box<dyn Error>> {
     let input_path = env::args().skip(1).next().expect("usage: elk FILE");
     let input = fs::read(&input_path)?;
-    delf::File::parse(&input[..]).map_err(|e| format!("{:?}", e))?;
-    println!("input is a supported ELF file");
+    let (_, file) = delf::File::parse(&input[..]).map_err(|e| format!("{:?}", e))?;
+    println!("{:#?}", file);
     Ok(())
 }
