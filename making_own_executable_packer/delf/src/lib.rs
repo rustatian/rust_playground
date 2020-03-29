3fa76c6a51f03be3a230d2bbe72fe943a8e0ceeb
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-03-29 23:57:32 +0300

    eppic fail Stefano

--- making_own_executable_packer/delf/src/lib.rs
+++ making_own_executable_packer/delf/src/lib.rs
@@ -1,10 +1,11 @@
 mod parse;
 
+use std::fmt;
+use std::fmt::{Formatter, Debug};
+use derive_more::*;
+
 // &'a [u8] is self.0
 pub struct HexDump<'a>(&'a [u8]);
-
-use std::fmt;
-use std::fmt::Formatter;
 
 impl<'a> fmt::Debug for HexDump<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
@@ -18,8 +19,13 @@ impl<'a> fmt::Debug for HexDump<'a> {
 
 #[derive(Debug)]
 pub struct File {
+    // offset: 0x10 2-bytes e_type
     pub r#type: Type,
+    // offset: 0x12 2-bytes e_machine
     pub machine: Machine,
+    // offset: 0x18 8-bytes e_entry
+    pub entry_point: Addr,
+
 }
 
 impl File {
@@ -27,11 +33,11 @@ impl File {
 
     pub fn parse(i: parse::Input) -> parse::Result<Self> {
         use nom::{
+            combinator::verify,
+            number::complete::le_u32,
             bytes::complete::{tag, take},
             error::context,
             sequence::tuple,
-            combinator::map,
-            number::complete::le_u16,
         };
         // 7f45 4c46 0201 0100 0000 0000 0000 0000
         let (i, _) = tuple((
@@ -43,15 +49,14 @@ impl File {
             context("Padding", take(8_usize)),
         ))(i)?;
 
-        let (i, (r#type, machine)) = tuple((
-            context("Type", map(le_u16, Type::from_u16)),
-            context("Machine", map(le_u16, Machine::from_u16)),
-        ))(i)?;
+        let (i, (r#type, machine)) = tuple((Type::parse, Machine::parse))(i)?;
 
-        let t = r#type;
-        let m = machine;
+        // offset: 0x14 4-bytes e_version
+        let (i, _) = context("Version (bis)", verify(le_u32, |&x| x == 1))(i)?;
 
-        let res = Self { machine: m.unwrap(), r#type: t.unwrap() };
+        let (i, entry_point) = Addr::parse(i)?;
+
+        let res = Self { machine, r#type, entry_point };
         Ok((i, res))
     }
 
@@ -76,6 +81,47 @@ impl File {
     }
 }
 
+// Addr is 8-bytes (64 bit platform) long
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Add, Sub)]
+pub struct Addr(pub u64); // tuple struct with 1 element, NEWTYPE pattern
+
+impl fmt::Debug for Addr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{:08x}", self.0)
+    }
+}
+
+impl fmt::Display for Addr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+impl Into<u64> for Addr {
+    fn into(self) -> u64 {
+        self.0
+    }
+}
+
+impl Into<usize> for Addr {
+    fn into(self) -> usize {
+        self.0 as usize
+    }
+}
+
+impl From<u64> for Addr {
+    fn from(x: u64) -> Self {
+        Self(x)
+    }
+}
+
+impl Addr {
+    pub fn parse(i: parse::Input) -> parse::Result<Self> {
+        use nom::{combinator::map, number::complete::le_u64};
+        map(le_u64, Addr::from)(i)
+    }
+}
+
 // e-type, 16 byte
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 #[repr(u16)]
@@ -88,6 +134,16 @@ pub enum Type {
 }
 
 impl Type {
+    pub fn parse(i: parse::Input) -> parse::Result<Self> {
+        use nom::{
+            error::{context, ErrorKind},
+            number::complete::le_u16,
+            combinator::map_res,
+        };
+
+        context("Type", map_res(le_u16, |x| Type::from_u16(x).ok_or(ErrorKind::Alt)))(i)
+    }
+
     pub fn from_u16(x: u16) -> Option<Self> {
         match x {
             0 => Some(Self::None),
@@ -108,43 +164,43 @@ pub enum Machine {
     X86_64 = 0x3e,
 }
 
-impl_parse_for_enum!(Type, le_u16);
-impl_parse_for_enum!(Machine, le_u16);
+// impl_parse_for_enum!(Type, le_u16);
+// impl_parse_for_enum!(Machine, le_u16);
 
 impl Machine {
-    // pub fn parse(i: parse::Input) -> parse::Result<Self> {
-    //     use nom::{
-    //         error::{context, ErrorKind},
-    //         number::complete::le_u16,
-    //         combinator::map_res,
-    //     };
-    //
-    //     // SOLUTION 1
-    //
-    //     // let (i, x) = le_u16(i)?;
-    //     // match Self::from_u16(x) {
-    //     //     None => {
-    //     //         Err(Err::Failure(VerboseError::from_error_kind(
-    //     //             original_i,
-    //     //             ErrorKind::Alt,)))
-    //     //     },
-    //     //     Some(res) => {
-    //     //         Ok((i, res))
-    //     //     },
-    //     // }
-    //
-    //     // SOLUTION 2 anti-pattern, option<t> convert to the Result<T,E>
-    //
-    //     // Returns Result<O2, E2>, where O2 is output of our mapping func
-    //     // and E2 is the error
-    //     // map_res(le_u16, |x| match Self::from_u16(x) {
-    //     //     Some(x) => Ok(x),
-    //     //     None => Err(ErrorKind::Alt),
-    //     // })(i)
-    //
-    //     // SOLUTION 3
-    //     context("Machine", map_res(le_u16, |x| Self::from_u16(x).ok_or(ErrorKind::Alt)))(i)
-    // }
+    pub fn parse(i: parse::Input) -> parse::Result<Self> {
+        use nom::{
+            error::{context, ErrorKind},
+            number::complete::le_u16,
+            combinator::map_res,
+        };
+
+        // SOLUTION 1
+
+        // let (i, x) = le_u16(i)?;
+        // match Self::from_u16(x) {
+        //     None => {
+        //         Err(Err::Failure(VerboseError::from_error_kind(
+        //             original_i,
+        //             ErrorKind::Alt,)))
+        //     },
+        //     Some(res) => {
+        //         Ok((i, res))
+        //     },
+        // }
+
+        // SOLUTION 2 anti-pattern, option<t> convert to the Result<T,E>
+
+        // Returns Result<O2, E2>, where O2 is output of our mapping func
+        // and E2 is the error
+        // map_res(le_u16, |x| match Self::from_u16(x) {
+        //     Some(x) => Ok(x),
+        //     None => Err(ErrorKind::Alt),
+        // })(i)
+
+        // SOLUTION 3
+        context("Machine", map_res(le_u16, |x| Self::from_u16(x).ok_or(ErrorKind::Alt)))(i)
+    }
     pub fn from_u16(m: u16) -> Option<Self> {
         match m {
             0x03 => Some(Self::X86),
