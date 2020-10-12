29baaf6dff5ae0c17085a3569592123d5ea9034e
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-10-12 23:17:12 +0300

    fixed the israeli-palestinian conflict

--- pingcap/kvs/src/lib.rs
+++ pingcap/kvs/src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![allow(unused_variables)]
+#[allow(unused_mut)]
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> Self {
+        KvStore {}
     }
+
+    pub fn set(&self, key: String, value: String) {}
+    pub fn get(&self, key: String) -> Option<String> {
+        None
+    }
+    pub fn remove(&self, key: String) {}
 }
