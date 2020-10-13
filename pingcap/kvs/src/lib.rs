61b0daf8865e0610b0f478774b479083c7fe2411
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-10-14 00:18:46 +0300

    more fixes

--- pingcap/kvs/src/lib.rs
+++ pingcap/kvs/src/lib.rs
@@ -1,15 +1,26 @@
-#![allow(unused_variables)]
-#[allow(unused_mut)]
-pub struct KvStore {}
+use std::collections::HashMap;
+
+pub struct KvStore {
+    values: HashMap<String, String>,
+}
 
 impl KvStore {
     pub fn new() -> Self {
-        KvStore {}
+        KvStore {
+            values: HashMap::new(),
+        }
     }
 
-    pub fn set(&self, key: String, value: String) {}
+    pub fn set(&mut self, key: String, value: String) {
+        self.values.insert(key, value);
+    }
     pub fn get(&self, key: String) -> Option<String> {
+        if let Some(val) = self.values.get(&key) {
+            return Option::from(val.to_owned());
+        }
         None
     }
-    pub fn remove(&self, key: String) {}
+    pub fn remove(&mut self, key: String) {
+        self.values.remove(&key);
+    }
 }
