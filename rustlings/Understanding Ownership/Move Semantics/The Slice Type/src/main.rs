eac59b013e93fd5d256ad5f69f53a67533126231
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-07 15:59:48 +0300

    bla

--- rustlings/Understanding Ownership/Move Semantics/The Slice Type/src/main.rs
+++ rustlings/Understanding Ownership/Move Semantics/The Slice Type/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let mut s = String::from("hello world");
-    let word = first_word(&s);
+    let word = first_word(&s[..]);
     s.clear();
     // word here is invalid
 
@@ -10,10 +10,8 @@ fn main() {
     // println!("{}", word); error
 }
 
-fn first_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
+fn first_word(s: &str) -> &str {
+    for (i, &item) in s.as_bytes().iter().enumerate() {
         if item == b' ' {
             return &s[0..i];
         }
