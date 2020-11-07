53b027702d811c9219feec129c9b9f10f9b67e81
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-07 18:50:40 +0300

    Now it's all microservices, I hope the fad persists.

--- rustlings/Modules and Macros/Modules/Intro/src/main.rs
+++ rustlings/Modules and Macros/Modules/Intro/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     // put you code here to launch it
+    font_of_house::hosting::add_to_waitlist();
+}
+
+
+mod font_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+    }
 }
