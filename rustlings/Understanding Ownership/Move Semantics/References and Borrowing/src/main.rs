13fcf9117672dda25442d78e8ce8640e74ecf93f
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-07 15:30:46 +0300

    Herping the derp derp (silly scoping error)

--- rustlings/Understanding Ownership/Move Semantics/References and Borrowing/src/main.rs
+++ rustlings/Understanding Ownership/Move Semantics/References and Borrowing/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     let s1 = String::from("hello");
-    let len = calculate_len(s1);
+    let len = calculate_len(&s1);
     println!("The length of {} is {}.", s1, len);
 }
 
-fn calculate_len(s: String) -> usize {
+fn calculate_len(s: &String) -> usize {
     let length = s.len();
     length
 }
