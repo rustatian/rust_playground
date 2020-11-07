2d89bd27f4b2bb52d69d337ff1a7fa985b9931e1
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-07 15:29:39 +0300

    Derpy hooves

--- rustlings/Understanding Ownership/Move Semantics/References and Borrowing/src/main.rs
+++ rustlings/Understanding Ownership/Move Semantics/References and Borrowing/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    // put you code here to launch it
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_len(s1);
+    println!("The length of {} is {}.", s2, len);
+}
+
+fn calculate_len(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
