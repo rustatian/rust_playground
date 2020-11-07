628577162dcaf7b03f5e105bfb39ca8552334b70
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-07 15:47:58 +0300

    put code that worked where the code that didn't used to be

--- rustlings/Understanding Ownership/Move Semantics/The Slice Type/src/main.rs
+++ rustlings/Understanding Ownership/Move Semantics/The Slice Type/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     // put you code here to launch it
 }
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
+}
