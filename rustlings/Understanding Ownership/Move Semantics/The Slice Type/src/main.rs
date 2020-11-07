38bb6b27e6ede2cc45d96f0d6024e228c8fa48d4
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-07 15:53:23 +0300

    Trying to fake a conflict

--- rustlings/Understanding Ownership/Move Semantics/The Slice Type/src/main.rs
+++ rustlings/Understanding Ownership/Move Semantics/The Slice Type/src/main.rs
@@ -3,15 +3,20 @@ fn main() {
     let word = first_word(&s);
     s.clear();
     // word here is invalid
+
+    let s2 = String::from("hello world");
+    let hello = &s2[0..5];
+    let world = &s2[6..11];
+    println!("{}", word);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
-    s.len()
+    &s[..]
 }
