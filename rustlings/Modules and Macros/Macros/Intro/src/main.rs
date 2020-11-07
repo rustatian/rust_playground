4ae8ab6418e9543706f53b3f403227b2fcce6033
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-07 20:58:17 +0300

    and a comma

--- rustlings/Modules and Macros/Macros/Intro/src/main.rs
+++ rustlings/Modules and Macros/Macros/Intro/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    // put you code here to launch it
+#[macro_export]
+macro_rules! vec2 {
+    ($($x:expr), *) => {
+        {
+           let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
 }
+
+fn main() {
+    let v: Vec<u32> = vec![1, 2, 3];
+    println!("{:?}", v);
+}
+
