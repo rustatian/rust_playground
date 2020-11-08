64e20d96d6a4964f6bd30c55f1d7fbb3f2eb6637
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-09 00:52:13 +0300

    fixed errors in the previous commit

--- rustlings/Recoverable and Unrecoverable Errors/Error Handling/Matching Result 2/src/main.rs
+++ rustlings/Recoverable and Unrecoverable Errors/Error Handling/Matching Result 2/src/main.rs
@@ -4,7 +4,14 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = match total_cost(pretend_user_input)?;
+    let cost = match total_cost(pretend_user_input) {
+        Ok(num) => {
+            num
+        }
+        Err(_) => {
+            panic!("")
+        }
+    };
 
     if cost > tokens {
         println!("You can't afford that many!");
