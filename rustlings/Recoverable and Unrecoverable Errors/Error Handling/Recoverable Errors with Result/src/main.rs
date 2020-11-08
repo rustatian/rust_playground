174eb2e94a98210827511f6dc375c2358e4655b2
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-08 21:22:18 +0300

    Here be Dragons

--- rustlings/Recoverable and Unrecoverable Errors/Error Handling/Recoverable Errors with Result/src/main.rs
+++ rustlings/Recoverable and Unrecoverable Errors/Error Handling/Recoverable Errors with Result/src/main.rs
@@ -1,3 +1,32 @@
+use std::io::{ErrorKind};
+use std::fs::File;
+
 fn main() {
-    // put you code here to launch it
+    // let f = std::fs::File::open("hello.txt");
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(err) => match err.kind() {
+    //         ErrorKind::NotFound => match std::fs::File::create("hello.txt") {
+    //             Ok(fs) => {
+    //                 fs
+    //             }
+    //             Err(err) => {
+    //                 panic!("problem creating the file: {:?}", err);
+    //             }
+    //         },
+    //         other_error => {
+    //             panic!("problem opening the file: {:?}", other_error)
+    //         }
+    //     },
+    // };
+
+    let f = std::fs::File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("error creating the file");
+            })
+        } else {
+            panic!("error opening");
+        }
+    });
 }
