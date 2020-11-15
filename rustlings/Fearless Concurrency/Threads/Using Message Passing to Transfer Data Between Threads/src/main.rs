a49567f00ef6739348bf5884218acb33dd364129
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-15 23:48:55 +0300

    Merge pull my finger request

--- rustlings/Fearless Concurrency/Threads/Using Message Passing to Transfer Data Between Threads/src/main.rs
+++ rustlings/Fearless Concurrency/Threads/Using Message Passing to Transfer Data Between Threads/src/main.rs
@@ -1,6 +1,11 @@
 use std::sync::mpsc;
+use std::thread;
 
 fn main() {
     // mpsc - multiple producer single consumer
     let (tx, rx) = mpsc::channel();
+    thread::spawn(|| {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
 }
