941c0803c5a1445b67f78d91ef0151ce0e1985b2
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-15 17:21:55 +0300

    bug fix

--- rustlings/Iterators and Closures/Standard Library Types/Intro/src/main.rs
+++ rustlings/Iterators and Closures/Standard Library Types/Intro/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     let v1 = vec![1, 2, 3];
-    let v1_iter = v1.iter();
+    let v1_clone = v1.clone();
+    let v1_iter = v1_clone.iter();
+
+    let mut v2_iter = v1.iter();
 
     for val in v1_iter {
         println!("Got: {}", val);
     }
+
+    println!("{}", v2_iter.next().unwrap());
 }
