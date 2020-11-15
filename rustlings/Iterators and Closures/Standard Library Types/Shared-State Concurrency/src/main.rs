947e7fc62c6ff5f61a348b2ca04509ec6b5774e8
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-15 20:20:18 +0300

    fixed mistaken bug

--- rustlings/Iterators and Closures/Standard Library Types/Shared-State Concurrency/src/main.rs
+++ rustlings/Iterators and Closures/Standard Library Types/Shared-State Concurrency/src/main.rs
@@ -1,12 +1,11 @@
-use std::sync::Mutex;
-use std::rc::Rc;
+use std::sync::{Mutex, Arc};
 
 fn main() {
-    let counter = Rc::new(Mutex::new(0));
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
-        let counter = Rc::clone(&counter);
+        let counter = Arc::clone(&counter);
         let handle = std::thread::spawn(move || {
             let mut num = counter.lock().unwrap();
             *num += 1;
