8dcab7b04a06e8525cd21636f8b97f094e9695ef
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-15 22:39:05 +0300

    Merge pull my finger request

--- rustlings/Fearless Concurrency/Threads/Intro/src/main.rs
+++ rustlings/Fearless Concurrency/Threads/Intro/src/main.rs
@@ -1,3 +1,15 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    // put you code here to launch it
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("{}", i);
+            thread::sleep(Duration::from_millis(10));
+        }
+    });
+    for i in 1..5 {
+        println!("{}", i);
+        thread::sleep(Duration::from_millis(10));
+    }
 }
