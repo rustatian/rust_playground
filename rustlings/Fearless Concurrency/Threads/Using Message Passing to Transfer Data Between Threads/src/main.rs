9f5b04092d899a120e31bd9f5ec481988d859c06
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-15 23:53:52 +0300

    REALLY FUCKING FIXED

--- rustlings/Fearless Concurrency/Threads/Using Message Passing to Transfer Data Between Threads/src/main.rs
+++ rustlings/Fearless Concurrency/Threads/Using Message Passing to Transfer Data Between Threads/src/main.rs
@@ -1,13 +1,24 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     // mpsc - multiple producer single consumer
     let (tx, rx) = mpsc::channel();
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1000));
+        }
     });
 
-    println!("{}", rx.recv().unwrap());
+    for rec in rx {
+        println!("{}", rec);
+    }
 }
