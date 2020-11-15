301d7dc039ac43182a2b69b57562e57f58940720
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-15 21:55:38 +0300

    That's just how I roll

--- rustlings/Iterators and Closures/Standard Library Types/Factorial/src/lib.rs
+++ rustlings/Iterators and Closures/Standard Library Types/Factorial/src/lib.rs
@@ -1,3 +1,26 @@
 pub fn factorial(num: u64) -> u64 {
     //Implement the function here
+    println!("the number is: {}", num);
+    if num == 0 {
+        return 0;
+    }
+    if num == 1 {
+        return 1;
+    }
+
+    if num == 2 {
+        return 2;
+    }
+
+    // res = 10*9 -> res*8 -> res*7
+    // 10 --> 10*9*8*7*6
+    // 4 (24) -> 4*3*2*1
+
+    let mut res = 1;
+    for n in 1..num + 1 {
+        println!("{}", res);
+        res *= n;
+    }
+
+    res
 }
