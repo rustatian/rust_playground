3196eb64fc2034fa86178c8926ec1474aa02e85c
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-15 19:08:28 +0300

    Useful text

--- rustlings/Iterators and Closures/Standard Library Types/Intro/src/main.rs
+++ rustlings/Iterators and Closures/Standard Library Types/Intro/src/main.rs
@@ -3,3 +3,12 @@ fn main() {
     let c: Vec<_> = v1.iter().map(|x| x + 1).collect();
     assert_eq!(c, vec![2, 3, 4]);
 }
+
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
