f2987a0bb57e64c0cb45d8e80683ae47e99695a8
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-15 20:18:43 +0300

    We Had To Use Dark Magic To Make This Work

--- rustlings/Iterators and Closures/Standard Library Types/Shared-State Concurrency/src/main.rs
+++ rustlings/Iterators and Closures/Standard Library Types/Shared-State Concurrency/src/main.rs
@@ -1,11 +1,22 @@
 use std::sync::Mutex;
+use std::rc::Rc;
 
 fn main() {
-    let m = Mutex::new(5);
-    {
-        let mut num = m.lock().unwrap();
-        *num = 6;
+    let counter = Rc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Rc::clone(&counter);
+        let handle = std::thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
     }
 
-    println!("m = {:?}", m);
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
