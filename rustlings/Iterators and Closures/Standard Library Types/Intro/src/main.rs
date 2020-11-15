d152305df5043e94269637c28865b3ce2b2a82b6
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-15 18:35:41 +0300

    NSA backdoor - ignore

--- rustlings/Iterators and Closures/Standard Library Types/Intro/src/main.rs
+++ rustlings/Iterators and Closures/Standard Library Types/Intro/src/main.rs
@@ -1,15 +1,5 @@
 fn main() {
     let v1 = vec![1, 2, 3];
-    let v1_iter = v1.iter();
-
-
-    for val in v1_iter {
-        println!("Got: {}", val);
-    }
-
-    let v2_iter = v1.into_iter();
-    for mut val in v2_iter {
-        val = 4;
-    }
-    println!("{:?}", "fasd");
+    let c: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    assert_eq!(c, vec![2, 3, 4]);
 }
