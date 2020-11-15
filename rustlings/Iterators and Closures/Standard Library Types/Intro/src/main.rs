5a58183960102be2cc847ebd9aeb795c27a94ea4
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-15 19:13:02 +0300

    Whatever.

--- rustlings/Iterators and Closures/Standard Library Types/Intro/src/main.rs
+++ rustlings/Iterators and Closures/Standard Library Types/Intro/src/main.rs
@@ -12,3 +12,40 @@ struct Shoe {
 fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Self {
+        Counter {
+            count: 0,
+        }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+}
