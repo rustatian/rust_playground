1fc21445729be9badef47cd419e5b5cdbbd11d06
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-15 22:57:08 +0300

    hoo boy

--- rustlings/Fearless Concurrency/Threads/Intro/src/main.rs
+++ rustlings/Fearless Concurrency/Threads/Intro/src/main.rs
@@ -2,15 +2,9 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("{}", i);
-            thread::sleep(Duration::from_millis(10));
-        }
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        println!("{:?}", v);
     });
-    for i in 1..5 {
-        println!("{}", i);
-        thread::sleep(Duration::from_millis(10));
-    }
     handle.join().unwrap();
 }
