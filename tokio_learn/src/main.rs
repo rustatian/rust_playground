6fe47b589002b0d9d034a528fdbba3accda83cc7
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-20 15:37:33 +0300

    If it's stupid and it works, it ain't stupid

--- tokio_learn/src/main.rs
+++ tokio_learn/src/main.rs
@@ -4,5 +4,20 @@ use tokio::net::{TcpListener, TcpStream};
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+
+        // respond with the error
+        let response = Frame::Error("unimplemented".into());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
