90a8a19b64cf47d3c6ad77bcc04c59b1ae0021eb
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-21 03:35:37 +0300

    Committed some changes

--- tokio_learn/examples/framing.rs
+++ tokio_learn/examples/framing.rs
@@ -1,3 +1,25 @@
-fn main() {
-    
+use tokio::net::TcpStream;
+use mini_redis::{Result, Frame};
+use bytes::BytesMut;
+
+fn main() {}
+
+struct Connection {
+    stream: TcpStream,
+    bytes: BytesMut,
+}
+
+impl Connection {
+    pub fn new(stream: TcpStream) -> Self {
+        Connection {
+            stream,
+            bytes: BytesMut::with_capacity(4096),
+        }
+    }
+    pub async fn read_frame(&mut self) -> Result<Option<Frame>> {
+        Ok(None)
+    }
+    pub async fn write_frame(&mut self) -> Result<()> {
+        Ok(())
+    }
 }
