60f5f88151d8ddaa9a669ab99c2d28d55795b83e
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-21 23:18:19 +0300

    For real, this time.

--- tokio_learn/examples/mini_tokio.rs
+++ tokio_learn/examples/mini_tokio.rs
@@ -1,45 +1,83 @@
+use crossbeam::channel;
+use futures::task;
+use futures::task::ArcWake;
 use std::collections::VecDeque;
 use std::future::Future;
 use std::pin::Pin;
+use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll};
-use futures::task;
 
-type Task = Pin<Box<dyn Future<Output=()> + Send>>;
+// type Task = Pin<Box<dyn Future<Output=()> + Send>>;
 
 fn main() {}
 
 struct MiniTokio {
-    tasks: VecDeque<Task>
+    scheduler: channel::Receiver<Arc<Task>>,
+    sender: channel::Sender<Arc<Task>>,
+}
+
+struct Task {
+    // The `Mutex` is to make `Task` implement `Sync`. Only
+    // one thread accesses `future` at any given time. The
+    // `Mutex` is not required for correctness. Real Tokio
+    // does not use a mutex here, but real Tokio has
+    // more lines of code than can fit in a single tutorial
+    // page.
+    future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>>,
+    executor: channel::Sender<Arc<Task>>,
+}
+
+impl Task {
+    fn schedule(self: &Arc<Self>) {
+        self.executor.send(self.clone());
+    }
+    fn poll(self: Arc<Self>) {
+        // create a waker from the Task
+        // self uses ArcWake impl
+        let waker = task::waker(self);
+        let mut cx = Context::from_waker(&waker);
+
+        // no other threads tries to lock the future
+        let mut future = self.future.try_lock().unwrap();
+        // poll the future
+        let _ = future.as_mut().poll(&mut cx);
+    }
+    fn spawn<F>(future: F, sender: &channel::Sender<Arc<Task>>)
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        let task = Arc::new(Task {
+            future: Mutex::new(Box::pin(future)),
+            executor: sender.clone(),
+        });
+
+        let _ = sender.send(task);
+    }
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        arc_self.schedule();
+    }
 }
 
 impl MiniTokio {
     fn new() -> Self {
-        MiniTokio {
-            tasks: VecDeque::new(),
-        }
+        let (sender, scheduler) = channel::unbounded();
+        MiniTokio { scheduler, sender }
     }
 
     // spawn a future onto the mini-tokio instance
     fn spawn<F>(&mut self, future: F)
-        where
-            F: Future<Output=()> + Send + 'static,
+    where
+        F: Future<Output = ()> + Send + 'static,
     {
-        self.tasks.push_back(Box::pin(future));
     }
 
     // this approach will burn CPU as we polling every future in the queue
     fn run(&mut self) {
-        // create noop waker
-        let waker = task::noop_waker();
-        // create context from waker
-        let mut cx = Context::from_waker(&waker);
-
-        // get task from the vec
-        while let Some(mut task) = self.tasks.pop_front() {
-            // if task is not ready -> push it back
-            if task.as_mut().poll(&mut cx).is_pending() {
-                self.tasks.push_back(task);
-            }
+        while let Ok(task) = self.scheduler.recv() {
+            task.poll();
         }
     }
 }
