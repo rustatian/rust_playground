0dd5d2478b631f015e54f64062136c8e4dadf20a
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-21 19:15:38 +0300

    Glue. Match sticks. Paper. Build script!

--- tokio_learn/examples/async_in_depth.rs
+++ tokio_learn/examples/async_in_depth.rs
@@ -30,3 +30,44 @@ impl Future for Delay {
     }
 }
 
+enum MainFuture {
+    // initialized
+    State0,
+    //waiting on delay, future.await line
+    State1(Delay),
+    // terminated
+    Terminated,
+}
+
+impl Future for MainFuture {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        use MainFuture::*;
+        loop {
+            match *self {
+                State0 => {
+                    let when = Instant::now() + Duration::from_secs(1);
+                    let future = Delay { when };
+                    *self = State1(future);
+                }
+                State1(ref mut my_future) => {
+                    return match Pin::new(my_future).poll(cx) {
+                        Poll::Ready(out) => {
+                            assert_eq!(out, "done");
+                            *self = Terminated;
+                            Poll::Ready(())
+                        }
+                        Poll::Pending => {
+                            Poll::Pending
+                        }
+                    }
+                }
+                Terminated => {
+                    panic!("future polled after competition");
+                }
+            }
+        }
+    }
+}
+
