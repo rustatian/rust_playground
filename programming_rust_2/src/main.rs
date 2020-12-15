77c1ad6566a0ad3fa08691c046b28c9de14cb530
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-12-15 21:27:14 +0300

    No changes after this point.

--- programming_rust_2/src/main.rs
+++ programming_rust_2/src/main.rs
@@ -4,23 +4,27 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn process_files_in_parallel(filenames: Vec<String>) -> io::Result<()> {
-    const NTHREADS: usize = 8;
-    let worklists = split_vec_into_cunks(filenames, NTHREADS);
+// fn process_files_in_parallel(filenames: Vec<String>) -> io::Result<()> {
+//     const NTHREADS: usize = 8;
+//     let worklists = split_vec_into_cunks(filenames, NTHREADS);
+//
+//     // fork: spawn a thread to handle each chunk
+//     let mut thread_handles = vec![];
+//     for worklist in worklists {
+//         thread_handles.push(
+//             std::thread::spawn(|| {
+//                 println!("hello");
+//             })
+//         );
+//     }
+//
+//     for handle in thread_handles {
+//         handle.join().unwrap()?;
+//     }
+//
+//     Ok(())
+// }
 
-    // fork: spawn a thread to handle each chunk
-    let mut thread_handles = vec![];
-    for worklist in worklists {
-        thread_handles.push(
-            std::thread::spawn(|| {
-                println!("hello");
-            })
-        );
-    }
-
-    for handle in thread_handles {
-        handle.join().unwrap()?;
-    }
-
-    Ok(())
+fn triangle(n: i32) -> i32 {
+    (1..n + 1).fold(0, |sum, item| sum + item)
 }
