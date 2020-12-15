cb6045b5e9b139121d4550ae8464a08edd4a830d
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-12-15 21:31:55 +0300

    apparently i did something…

--- programming_rust_2/src/main.rs
+++ programming_rust_2/src/main.rs
@@ -1,29 +1,35 @@
 use std::io;
+use std::collections::HashMap;
 
 fn main() {
     println!("Hello, world!");
 }
 
-// fn process_files_in_parallel(filenames: Vec<String>) -> io::Result<()> {
-//     const NTHREADS: usize = 8;
-//     let worklists = split_vec_into_cunks(filenames, NTHREADS);
-//
-//     // fork: spawn a thread to handle each chunk
-//     let mut thread_handles = vec![];
-//     for worklist in worklists {
-//         thread_handles.push(
-//             std::thread::spawn(|| {
-//                 println!("hello");
-//             })
-//         );
-//     }
-//
-//     for handle in thread_handles {
-//         handle.join().unwrap()?;
-//     }
-//
-//     Ok(())
-// }
+// just stub
+fn process_files(filenames: Vec<String>, glossary_child: &HashMap<i32, i32>) {}
+
+fn process_files_in_parallel(filenames: Vec<String>, glossary: std::sync::Arc<HashMap<i32, i32>>) -> io::Result<()> {
+    const NTHREADS: usize = 8;
+    // let worklists = split_vec_into_cunks(filenames, NTHREADS);
+    let worklists: Vec<Vec<String>> = vec![];
+
+    // fork: spawn a thread to handle each chunk
+    let mut thread_handles = vec![];
+    for worklist in worklists {
+        let glossary_clone = glossary.clone();
+        thread_handles.push(
+            std::thread::spawn(move || {
+                process_files(worklist, &glossary_clone);
+            })
+        );
+    }
+
+    for handle in thread_handles {
+        handle.join().unwrap();
+    }
+
+    Ok(())
+}
 
 fn triangle(n: i32) -> i32 {
     (1..n + 1).fold(0, |sum, item| sum + item)
