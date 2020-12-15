843536023680cca32b50c8f0bcda405ef8c373fd
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-12-15 20:32:26 +0300

    Merge pull request #67 from Lazersmoke/fix-andys-shit Fix andys shit

---
+++ programming_rust_2/src/main.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn process_files_in_parallel(filenames: Vec<String>) -> io::Result<()> {
+    const NTHREADS: usize = 8;
+    let worklists = split_vec_into_cunks(filenames, NTHREADS);
+
+    // fork: spawn a thread to handle each chunk
+    let mut thread_handles = vec![];
+    for worklist in worklists {
+        thread_handles.push(
+            std::thread::spawn(|| {
+                println!("hello");
+            })
+        );
+    }
+
+    for handle in thread_handles {
+        handle.join().unwrap()?;
+    }
+
+    Ok(())
+}
