03f0be49de9704075fdbfd3e88c79ebb76fc7f12
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-01-22 20:39:57 +0300

    Is there an award for this?

--- experiments/src/main.rs
+++ experiments/src/main.rs
@@ -1,11 +1,7 @@
-#[link(name = "")]
-extern "C" {
-    fn SetOpenOrClosed(a: i32) -> i32;
-}
-
 fn main() {
-    unsafe {
-        SetOpenOrClosed(1);
-    }
+
 }
 
+struct Structure<'a> {
+    reference: &'a u8
+}
