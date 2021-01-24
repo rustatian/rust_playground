4538989f6798d4c2fea5a315c0b33445303b8225
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-01-24 18:16:45 +0300

    If it's hacky and you know it clap you hands (clap clap)!

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,5 +1,6 @@
 extern crate num;
 use num::Complex;
+use std::str::FromStr;
 
 fn main() {
     println!("Hello, world!");
@@ -15,4 +16,14 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     }
 
     None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
 }
