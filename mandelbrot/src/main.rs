bc0617b603dcef6e2aa016e3b8fb481e97fd15ba
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-01-24 17:58:03 +0300

    lol

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,18 @@
+extern crate num;
+use num::Complex;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+
+    None
 }
