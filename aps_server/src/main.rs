6fb82aab762ae18523283b7f8157bdf5cb1094fa
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-03-20 14:26:28 +0300

    Spinning up the hamster...

--- aps_server/src/main.rs
+++ aps_server/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use serde_json::json;
+use uuid::Uuid;
+
+async fn devices(req: HttpRequest) -> impl Responder {
+    let mut related_dev_groups = vec![];
+    let mut tags = vec![];
+
+    for _ in 0..1000 {
+        related_dev_groups.push(Uuid::new_v4().to_string());
+    }
+
+    for _ in 0..1000 {
+        tags.push(Uuid::new_v4().to_string());
+    }
+
+    let value = json!({
+        "networkId": Uuid::new_v4().to_string(),
+        "relatedDeviceGroupIds": related_dev_groups,
+        "tags": tags,
+        "deviceType":Uuid::new_v4().to_string(),
+    });
+
+    value.to_string()
+}
+
+async fn networks(req: HttpRequest) -> impl Responder {
+    json!({
+    "parentId" : Uuid::new_v4().to_string(),
+    })
+    .to_string()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/api/v3/networks/devices/{uuid}", web::get().to(devices))
+            .route("api/v3/networks/{name}", web::get().to(networks))
+    })
+    .bind(("127.0.0.1", 3001))?
+    .workers(10000)
+    .run()
+    .await
 }
