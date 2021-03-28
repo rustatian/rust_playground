88205e9284b57c79c980fa6f5057cf9bbbd59f3c
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-03-28 21:24:02 +0300

    Added missing file in previous commit

--- pin-and-suffering/src/main.rs
+++ pin-and-suffering/src/main.rs
@@ -5,7 +5,9 @@ use std::task::{Context, Poll};
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let fut = MyFuture {};
+    println!("Awaiting fut...");
     fut.await;
+    println!("Awaiting fun...done!");
 }
 
 struct MyFuture {}
@@ -14,6 +16,6 @@ impl Future for MyFuture {
     type Output = ();
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        todo!()
+        Poll::Ready(())
     }
 }
