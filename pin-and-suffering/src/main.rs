28a047673f65014a64b9fba3aac3630351c94730
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-03-28 17:09:06 +0300

    final commit.

--- pin-and-suffering/src/main.rs
+++ pin-and-suffering/src/main.rs
@@ -1,4 +1,19 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    let fut = MyFuture {};
+    fut.await;
+}
 
+struct MyFuture {}
+
+impl Future for MyFuture {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        todo!()
+    }
 }
