f0384ed598109a1386f71d5042a518000f3dec2b
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-03-28 17:06:55 +0300

    mergederp

--- pin-and-suffering/src/main.rs
+++ pin-and-suffering/src/main.rs
@@ -1,16 +1,4 @@
-use std::time::Duration;
-use tokio::time::sleep;
-
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let one = tokio::spawn(greet());
-    let two = tokio::spawn(greet());
 
-    let (_, _) = tokio::join!(one, two);
-}
-
-async fn greet() {
-    println!("Hello!");
-    sleep(Duration::from_millis(500)).await;
-    println!("Good Bye!");
 }
