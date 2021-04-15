91deb60c3146a9a73b2a0cf57fcc957bd1bf43ed
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-04-15 22:50:35 +0300

    this is how we generate our shit.

--- rust_1/src/main.rs
+++ rust_1/src/main.rs
@@ -1,9 +1,17 @@
+static mut STASH: &i32 = &128;
+
 fn main() {
     let r = &factorial(6);
 
-    assert_eq!(r + &1009, 1729);
+    assert_eq!(r + &1009, 1129);
 }
 
 fn factorial(n: usize) -> usize {
     (1..n).fold(1, |a, b| a * b)
+}
+
+fn f(p: &'static i32) {
+    unsafe {
+        STASH = p;
+    }
 }
