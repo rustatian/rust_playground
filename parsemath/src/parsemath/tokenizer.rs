993ee0c1011eafa74e75219e69063fdd5ce0bbc0
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-05-04 23:41:50 +0300

    just checking if git is working properly...

--- parsemath/src/parsemath/tokenizer.rs
+++ parsemath/src/parsemath/tokenizer.rs
@@ -4,3 +4,11 @@ use std::str::Chars;
 pub struct Tokenizer<'a> {
     expr: Peekable<Chars<'a>>,
 }
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(new_expr: &'a str) -> Self {
+        Tokenizer {
+            expr: new_expr.chars().peekable(),
+        }
+    }
+}
