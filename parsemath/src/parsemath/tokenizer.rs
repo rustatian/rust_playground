c16ab4e2e8e7d4255801075a6c405badae6909ce
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-05-04 23:44:04 +0300

    needs more cow bell

--- parsemath/src/parsemath/tokenizer.rs
+++ parsemath/src/parsemath/tokenizer.rs
@@ -2,10 +2,12 @@ use std::iter::Peekable;
 use std::str::Chars;
 
 pub struct Tokenizer<'a> {
+    // peekable iterator over the string
     expr: Peekable<Chars<'a>>,
 }
 
 impl<'a> Tokenizer<'a> {
+    // creates a new tokenizer using the arithmetic expression provided by the user
     pub fn new(new_expr: &'a str) -> Self {
         Tokenizer {
             expr: new_expr.chars().peekable(),
