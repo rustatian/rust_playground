21dd2bd6f037bed96703f126ae8f81234d760501
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-05-08 23:56:02 +0300

    Derp search/replace fuckup

--- parsemath/src/parsemath/parser.rs
+++ parsemath/src/parsemath/parser.rs
@@ -1,10 +1,23 @@
+use crate::parsemath::token::Token;
+use crate::parsemath::tokenizer::Tokenizer;
 use std::string::ParseError;
 
-struct Parser {
+struct Parser<'a> {
+    tokenizer: Tokenizer<'a>,
+    current_token: Token,
 }
 
-impl Parser {
+impl<'a> Parser<'a> {
     fn new(expr: &'a str) -> Result<Self, ParseError> {
+        let mut lexer = Tokenizer::new(expr);
+        let cur_token = match lexer.next() {
+            Some(token) => token,
+            None => return Err(ParseError::Invalid),
+        };
 
+        Ok(Parser {
+            tokenizer: lexer,
+            current_token: cur_token,
+        })
     }
 }
