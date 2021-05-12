e0b8be91c8ed3c2f22443b827ebf8580cce4a64e
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-05-12 10:08:54 +0300

    I just evaluated random code in my console

--- async_streams/src/main.rs
+++ async_streams/src/main.rs
@@ -1,7 +1,7 @@
-use futures::{Future, Stream, StreamExt, stream};
+use futures::{stream, Future, Stream, StreamExt};
 use lazy_static::lazy_static;
 use rand::distributions::{Distribution, Uniform};
-use std::time::Duration;
+use std::{time::Duration, usize};
 use tokio::time::{sleep, Instant};
 
 lazy_static! {
@@ -10,24 +10,51 @@ lazy_static! {
 
 #[tokio::main]
 async fn main() {
-    println!("IDs from first 5 pages:\n{:?}",get_ids_n_pages(5).collect::<Vec<usize>>().await)
-    
+    println!(
+        "IDs from first 5 pages:\n{:?}",
+        get_ids_n_pages(5).collect::<Vec<usize>>().await
+    );
+    println!(
+        "IDs from first 10 pages, buffered by 5:\n{:?}",
+        get_ids_n_pages_buffered(10, 5)
+            .collect::<Vec<usize>>()
+            .await
+    );
+    println!(
+        "IDs from first 10 pages, buffered, unordered by 5:\n{:?}",
+        get_ids_n_pages_buffered(10, 5)
+            .collect::<Vec<usize>>()
+            .await
+    );
 }
 
-fn get_ids_n_pages(n:usize) -> impl Stream<Item = usize> {
+fn get_ids_n_pages(n: usize) -> impl Stream<Item = usize> {
     get_pages().take(n).flat_map(|page| stream::iter(page))
 }
 
+fn get_ids_n_pages_buffered(n: usize, buf_factor: usize) -> impl Stream<Item = usize> {
+    get_pages_futures()
+        .take(n)
+        .buffered(buf_factor)
+        .flat_map(|page: Vec<usize>| stream::iter(page))
+}
+
+fn get_ids_n_pages_buffered_unordered(n: usize, buf_factor: usize) -> impl Stream<Item = usize> {
+    get_pages_futures()
+        .take(n)
+        .buffer_unordered(buf_factor)
+        .flat_map(|page: Vec<usize>| stream::iter(page))
+}
 async fn get_n_pages(n: usize) -> Vec<Vec<usize>> {
     get_pages().take(n).collect().await
 }
 
 async fn get_n_pages_buffered(n: usize, buf_factor: usize) -> Vec<Vec<usize>> {
     get_pages_futures()
-    .buffer_unordered(buf_factor)
-    .take(n)
-    .collect()
-    .await
+        .buffer_unordered(buf_factor)
+        .take(n)
+        .collect()
+        .await
 }
 
 fn get_pages_buffered(buf_factor: usize) -> impl Stream<Item = Vec<usize>> {
@@ -70,14 +97,22 @@ impl std::fmt::Debug for Resource {
     }
 }
 
-
 async fn fetch_resource(i: usize) -> Resource {
     let millis = Uniform::from(0..10).sample(&mut rand::thread_rng());
-    println!("[{}] ## fetch_resource({}) will complete in {} ms", START_TIME.elapsed().as_millis(), i, millis);   
+    println!(
+        "[{}] ## fetch_resource({}) will complete in {} ms",
+        START_TIME.elapsed().as_millis(),
+        i,
+        millis
+    );
 
     sleep(Duration::from_millis(millis)).await;
 
-    println!("[{}] ## fetch_resource({}) completed", START_TIME.elapsed().as_millis(), i);
+    println!(
+        "[{}] ## fetch_resource({}) completed",
+        START_TIME.elapsed().as_millis(),
+        i
+    );
 
     Resource(i)
 }
