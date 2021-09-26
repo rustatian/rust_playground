87320021df670495d91905d723e2a572986efa4c
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-09-26 21:52:52 +0300

    To those I leave behind, good luck!

--- experiments/src/main.rs
+++ experiments/src/main.rs
@@ -1,6 +1,11 @@
 use std::io::Write;
 
 fn main() {
-    let mut buf:Vec<u8> = vec![];
+    let mut buf: Vec<u8> = vec![];
     let writer: &mut Write = &mut buf;
+}
+
+trait Visible {
+    fn draw(&self, canvas: &mut Canvas);
+    fn hit_set(&self, x: i32, y: i32) -> bool;
 }
