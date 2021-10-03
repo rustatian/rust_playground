7ced45fc18f7443d141f3183aeee5c9ee77d967b
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-10-03 23:11:02 +0300

    unionfind is no longer being molested.

--- experiments/src/main.rs
+++ experiments/src/main.rs
@@ -3,9 +3,14 @@ use std::io::Write;
 fn main() {
     let mut buf: Vec<u8> = vec![];
     let writer: &mut Write = &mut buf;
+
+    <str as ToString>::to_string("foo"); // fully qualified method name
+    let line = "foo foo";
+
+
+    let words: Vec<String> = line.split_whitespace().map(ToString::to_string).collect();
 }
 
 trait Visible {
-    fn draw(&self, canvas: &mut Canvas);
     fn hit_set(&self, x: i32, y: i32) -> bool;
 }
