9187ebc93666c00b7a0c43d9d97ca719a26cec68
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-10-04 10:34:51 +0300

    Put everything in its right place

--- experiments/src/main.rs
+++ experiments/src/main.rs
@@ -1,4 +1,6 @@
+#![allow(dead_code)]
 use std::io::Write;
+use std::iter::Iterator;
 
 fn main() {
     let mut buf: Vec<u8> = vec![];
@@ -7,10 +9,24 @@ fn main() {
     <str as ToString>::to_string("foo"); // fully qualified method name
     let line = "foo foo";
 
-
     let words: Vec<String> = line.split_whitespace().map(ToString::to_string).collect();
 }
 
 trait Visible {
     fn hit_set(&self, x: i32, y: i32) -> bool;
+}
+
+pub trait Iterator2 {
+    type Item; // associated type
+
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
+fn collect_into_vector<I: std::iter::Iterator>(iter: I) -> Vec<I::Item> {
+    let mut results = Vec::new();
+
+    for value in iter {
+        results.push(value);
+    }
+    results
 }
