d752d2cbcf2ac656353d7d07690c436753684a60
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-03-24 19:23:00 +0100

    new: wasm host<->guest work
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ projects/smartcms_test_host/src/main.rs
@@ -0,0 +1,46 @@
+wasmtime::component::bindgen!({
+    path: "./smart_cms.wit",
+    world: "app",
+});
+
+struct KeyValue {
+    mem: std::collections::HashMap<String, String>,
+}
+
+impl component::smartcms::kvstore::Host for KeyValue {
+    fn get(&mut self, key: String) -> String {
+        self.mem.get(&key).cloned().unwrap()
+    }
+
+    fn set(&mut self, key: String, value: String) {
+        self.mem.insert(key, value);
+    }
+}
+
+struct State {
+    key_value: KeyValue,
+}
+
+fn main() {
+    let mut config = wasmtime::Config::default();
+    config.wasm_component_model(true);
+    let engine = wasmtime::Engine::new(&config).unwrap();
+
+    let mut store = wasmtime::Store::new(
+        &engine,
+        State {
+            key_value: KeyValue {
+                mem: std::collections::HashMap::new(),
+            },
+        },
+    );
+    let component = wasmtime::component::Component::new(&engine, "guest.wasm").unwrap();
+    let mut linker = wasmtime::component::Linker::new(&engine);
+    component::smartcms::kvstore::add_to_linker(&mut linker, |state: &mut State| {
+        &mut state.key_value
+    })
+    .unwrap();
+
+    let instance = App::instantiate(&mut store, &component, &linker).unwrap();
+    println!("{:?}", instance.call_run(&mut store).unwrap());
+}
---
+++ projects/wasm/src/lib.rs
@@ -0,0 +1,18 @@
+#[unsafe(no_mangle)]
+pub fn greet(ptr: i32, len: i32) {
+    let hello = "Hello, ";
+    let input_ptr = ptr as *mut u8;
+    let input_len = len as usize;
+    let new_len = input_len + hello.len();
+    
+    let output = unsafe {
+        core::slice::from_raw_parts_mut(0 as *mut u8, new_len)
+    };
+    
+    output[..hello.len()].copy_from_slice(hello.as_bytes());
+    output[hello.len()..].copy_from_slice(
+        unsafe {
+            core::slice::from_raw_parts(input_ptr, input_len)
+        }
+    );
+}
